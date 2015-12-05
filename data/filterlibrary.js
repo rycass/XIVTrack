0716c7b62ca6560fdb620cf8e59d43f7c728caca
Rycass <rycass@gmail.com>
2015-12-05 17:24:17 -0500

    Triple Triad data entry
    Filled out Triple Triad cards, not in ID order yet because I can't find
    confirmation on ID order...

--- data/filterlibrary.js
+++ data/filterlibrary.js
@@ -2,7 +2,7 @@ filterLibrary = [
 			{ category: "Quest", subcategory: [{category:"Quests"}, {category:"FATEs"}, {category:"Treasure Hunt"}]} ,
 			{ category: "Crafted", subcategory: [{category:"ALC"}, {category:"ARM"}, {category:"BSM"}, {category:"CRP"}, {category:"CUL"}, {category:"GSM"}, {category:"LTW"}, {category:"WVR"}] },
 			{ category: "Gathered", subcategory: [{category:"MIN"}, {category:"BTN"}, {category:"FSH"}] },
-			{ category: "Group Content", subcategory: [{category:"Dungeon"}, {category:"Trial"}, {category:"Extreme Trial"}, {category:"Raid"}, {category:"Savage Raid"}, {category:"24-man Raid"}] },
+			{ category: "Group Content", subcategory: [{category:"Dungeon"}, {category:"Trial"}, {category:"Raid"}] },
 			{ category: "Vendor", subcategory: [{category:"Gil"}, {category:"Tomestones"}, {category:"MGP"}, {category:"Hunt Seals"}, {category: "Grand Company Seals"}, {category: "Other"}]},
 			{ category: "Promotional", subcategory: [{category: "Veteran"}, {category: "Mog Station"}, {category: "Recruit-a-Friend"}, {category: "Merchandise Bonus"}, {category: "Holiday/Mog Station"}]},
 			{ category: "Other", subcategory: [{category: "Venture"}, {category: "Desynthesis"}, {category: "Gardening"}, {category: "Achievement"}, {category: "NPC Only"}, {category:"Other"}]},
